b890cacf936d247a6de2e43ca7b0d16a7512537d
sanjanarajj97 <sanjana.rajj97@gmail.com>
2025-02-25 18:15:25 +0100

    build error

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,28 +1,41 @@
-import {defineQuery} from "groq";
+import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY =
-    defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || category match $search || author->name match $search || title match $search] | order(_createdAt desc){
-  _id, title, slug, _createdAt
-  , author ->{
-    _id, name, slug, image, bio,
-  }
-    
-    , views, description,
-    category, image 
+    defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+  _id, 
+  title, 
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, image, bio
+  }, 
+  views,
+  description,
+  category,
+  image,
 }`);
 
-export const STARTUP_BY_ID_QUERY = defineQuery(`*[_type == "startup" && _id == $id][0]{
-  _id, title, slug, _createdAt
-  , author ->{
-    _id, name, username, slug, image, bio,
-  }
-    
-    , views, description,
-    category, image , pitch
+export const STARTUP_BY_ID_QUERY =
+    defineQuery(`*[_type == "startup" && _id == $id][0]{
+  _id, 
+  title, 
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, username, image, bio
+  }, 
+  views,
+  description,
+  category,
+  image,
+  pitch,
 }`);
 
-export const STARTUP_VIEWS_QUERY=defineQuery(`*[_type == "startup" && _id == $id][0]{
-_id, views}`);
+export const STARTUP_VIEWS_QUERY = defineQuery(`
+    *[_type == "startup" && _id == $id][0]{
+        _id, views
+    }
+`);
 
 export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
 *[_type == "author" && id == $id][0]{
@@ -35,6 +48,7 @@ export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
     bio
 }
 `);
+
 export const AUTHOR_BY_ID_QUERY = defineQuery(`
 *[_type == "author" && _id == $id][0]{
     _id,
@@ -48,14 +62,18 @@ export const AUTHOR_BY_ID_QUERY = defineQuery(`
 `);
 
 export const STARTUPS_BY_AUTHOR_QUERY =
-    defineQuery(`*[_type == "startup" && author._ref == $id] | order(_createdAt desc){
-  _id, title, slug, _createdAt
-  , author ->{
-    _id, name, slug, image, bio,
-  }
-    
-    , views, description,
-    category, image 
+    defineQuery(`*[_type == "startup" && author._ref == $id] | order(_createdAt desc) {
+  _id, 
+  title, 
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, image, bio
+  }, 
+  views,
+  description,
+  category,
+  image,
 }`);
 
 export const PLAYLIST_BY_SLUG_QUERY =
